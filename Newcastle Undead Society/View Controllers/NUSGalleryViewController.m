82f149223c02bf57413dde22fadae9ba0070fcdc
Josh Lapham <josh@joshlapham.com>
2014-06-04 14:49:18 +1000

    Added images to Gallery VC.

--- Newcastle Undead Society/View Controllers/NUSGalleryViewController.m
+++ Newcastle Undead Society/View Controllers/NUSGalleryViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NUSGalleryViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface NUSGalleryViewController () {
     NSMutableArray *cellArray;
@@ -55,11 +56,25 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"GalleryTitleCell" forIndexPath:indexPath];
     
     UILabel *titleLabel = (UILabel *)[cell viewWithTag:102];
-    UIImageView *imageView = (UIImageView *)[cell viewWithTag:101];
+    UIImageView *cellImageView = (UIImageView *)[cell viewWithTag:101];
     
-    titleLabel.text = [cellArray objectAtIndex:indexPath.row];
+    // Scale image to fill cell
+    cellImageView.contentMode = UIViewContentModeScaleToFill;
     
-    NSLog(@"cell array: %@", [cellArray objectAtIndex:indexPath.row]);
+    // Set cell title
+    titleLabel.text = [[cellArray objectAtIndex:indexPath.row] objectForKey:@"title"];
+    
+    // Set cell thumbnail using SDWebImage
+    [cellImageView setImageWithURL:[NSURL URLWithString:[[cellArray objectAtIndex:indexPath.row] objectForKey:@"imageUrl"]]
+                                             placeholderImage:nil
+                                                    completed:^(UIImage *cellImage, NSError *error, SDImageCacheType cacheType) {
+                                                        if (cellImage && !error) {
+                                                            //DDLogVerbose(@"Comix: fetched comic thumbnail image");
+                                                        } else {
+                                                            //DDLogError(@"Comix: error fetching comic thumbnail image: %@", [error localizedDescription]);
+                                                        // TODO: implement fallback
+                                                        }
+                                                    }];
     
     return cell;
 }
@@ -76,14 +91,16 @@
     // Register cell with tableView
     [self.tableView registerNib:[UINib nibWithNibName:@"NUSGalleryTitleCell" bundle:nil] forCellReuseIdentifier:@"GalleryTitleCell"];
     
+    // Remove seperator insets from tableView
+    //[self.tableView setSeparatorInset:UIEdgeInsetsZero];
+    
     // Init cellArray data source
     [self initCellArrayDataSource];
-    
-    //[self.tableView set]
 }
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
+    // Init with grouped table style
     self = [super initWithStyle:UITableViewStyleGrouped];
     
     if (!self) {
@@ -98,10 +115,18 @@
 {
     cellArray = [[NSMutableArray alloc] init];
     
-    [cellArray addObject:@"2013"];
-    [cellArray addObject:@"2012"];
-    [cellArray addObject:@"2011"];
-    [cellArray addObject:@"2010"];
+    NSDictionary *galleryItem1 = @{@"title" : @"2013", @"imageUrl" : @"https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xfa1/t1.0-9/1453493_658726834172236_1271715398_n.jpg"};
+    NSDictionary *galleryItem2 = @{@"title" : @"2012", @"imageUrl" : @"https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xaf1/t1.0-9/536424_474541165924138_1749738981_n.jpg"};
+    NSDictionary *galleryItem3 = @{@"title" : @"2011", @"imageUrl" : @"https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xaf1/t1.0-9/315710_259496494095274_1257647121_n.jpg"};
+    // NOTE - actually using a 2009 image for 2010
+    NSDictionary *galleryItem4 = @{@"title" : @"2010", @"imageUrl" : @"https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xaf1/t1.0-9/262887_224862680891989_6260150_n.jpg"};
+    NSDictionary *galleryItem5 = @{@"title" : @"2009", @"imageUrl" : @"https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xaf1/t1.0-9/33518_127658320612426_6225652_n.jpg"};
+    
+    [cellArray addObject:galleryItem1];
+    [cellArray addObject:galleryItem2];
+    [cellArray addObject:galleryItem3];
+    [cellArray addObject:galleryItem4];
+    [cellArray addObject:galleryItem5];
 }
 
 @end
